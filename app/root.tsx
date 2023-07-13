d7d9f242b5e14d4ca401d3a26f422e5dc63a2425
jansa <jansa@DESKTOP-BMJT06E>
2023-07-13 21:18:30 +0300

    Coomit root MetaFunction

--- app/root.tsx
+++ app/root.tsx
@@ -7,3 +7,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react'
+
+export const meta: MetaFunction = () => ({
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+})

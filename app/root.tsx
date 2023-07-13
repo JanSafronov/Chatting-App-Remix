3bcc7a3b84202f2fa9746ee788781d1066efc141
jansa <jansa@DESKTOP-BMJT06E>
2023-07-13 21:19:13 +0300

    Commit App page initialization in root

--- app/root.tsx
+++ app/root.tsx
@@ -13,3 +13,20 @@ export const meta: MetaFunction = () => ({
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1',
 })
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
+}

f3beb2c5f61f7572e88cd6b1a72f0ac6868e9405
jansa <jansa@DESKTOP-BMJT06E>
2023-07-13 21:20:58 +0300

    Commit markup init in server and hydrate client

---
+++ app/entry.server.tsx
@@ -0,0 +1,16 @@
+import type { EntryContext } from '@remix-run/node'
+import { RemixServer } from '@remix-run/react'
+import { renderToString } from 'react-dom/server'
+
+export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  const markup = renderToString(
+    <RemixServer context={remixContext} url={request.url} />
+  )
+
+  
+}
